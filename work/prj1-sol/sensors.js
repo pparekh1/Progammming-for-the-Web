a0b127de845495bf97271c68a25d62f898204b9f
Pranjal Kamlesh Parekh <pparekh1@CS444-pparekh1.pods.bu.int>
2019-09-16 04:24:56 -0400

    done with sensor type and sensor

--- work/prj1-sol/sensors.js
+++ work/prj1-sol/sensors.js
@@ -4,8 +4,19 @@ const assert = require('assert');
 
 class Sensors {
 
+  
+
   constructor() {
     //@TODO
+     this.senType=[];
+     this.sen=[];
+     this.senData=[];
+     this.nextIndex=0;
+     this.noArgs=[];
+     this.sensorArr=[];
+     this.dataArr=[];
+     
+     this.type;
   }
 
   /** Clear out all data from this object. */
@@ -20,8 +31,33 @@ class Sensors {
    *  All user errors must be thrown as an array of objects.
    */
   async addSensorType(info) {
+    //sendata  =  info;
+    
+    
+    
+    
+
     const sensorType = validate('addSensorType', info);
+    
     //@TODO
+    //this.senType.push(info);
+    var mode=0;
+    for(var i=0; i<this.senType.length;i++){
+      if(this.senType[i].id===info.id ){
+        mode=1;
+      }
+      } 
+      if(mode===1){
+        console.log("Already present");
+      }
+      else {
+       
+        this.senType.push(info);
+      }
+    
+  
+    
+
   }
   
   /** Subject to field validation as per FN_INFOS.addSensor, add
@@ -32,6 +68,31 @@ class Sensors {
    */
   async addSensor(info) {
     const sensor = validate('addSensor', info);
+    // var nextIndex = DEFAULT_COUNT;
+    // this.sen.push(info);
+     var mode=0;
+    // for(var i=0;i<nextIndex;i++){
+    //   console.log(this.sen[i]);
+    // }
+    // console.log(this.sen.length);
+    // console.log(this.senType.length);
+    for(var i=0; i<this.senType.length;i++){
+
+     
+      if(this.senType[i].id===info.model){
+        mode=1;
+      }
+      } 
+    
+      if(mode===1){
+        this.sen.push(info);
+      }
+      // else {
+      //   console.log("absent");
+      // }
+    
+    
+    
     //@TODO
   }
 
@@ -44,7 +105,22 @@ class Sensors {
    */
   async addSensorData(info) {
     const sensorData = validate('addSensorData', info);
+    //this.senData.push(info);
     //@TODO
+    var mode=0;
+    for(var i=0; i<this.sen.length;i++){
+      if(this.sen[i].id===info.sensorId ){
+        mode=1;
+      }
+      } 
+      if(mode===1){
+        this.senData.push(info);
+      }
+      else {
+        
+        
+        throw "Absent";
+      }
   }
 
   /** Subject to validation of search-parameters in info as per
@@ -69,9 +145,96 @@ class Sensors {
    *  All user errors must be thrown as an array of objects.
    */
   async findSensorTypes(info) {
+    
     const searchSpecs = validate('findSensorTypes', info);
     //@TODO
-    return {};
+    //var allKeys;
+  //var allValues;
+    //var s = info;
+    // var allKeys= [];
+    // for( var k in info)
+    //   allKeys.push(k);
+    //allValues=Object.values(info);
+    //let allKeys=this.sentype.map(a =>a.id);
+    //console.log(allKeys);
+    
+    //console.log(this.senType);
+    var mode=0;
+    let noArgs=[];
+    var defaultArr=[];
+    var nextIndex=0;
+    this.senType.sort((a, b) => (a.id > b.id) ? 1 : -1)
+
+    /**********Default=5******* */
+    
+    //return noArgs;
+    // /*************ID************************/
+    // for(var i=0;i<this.senType.length;i++){
+    //   if(this.senType[i].id===info.id){
+    //     // console.log(this.senType[i]);
+    //   }
+    // }
+    // /*************Manufacturer************************/
+    // for(var i=0;i<this.senType.length;i++){
+    //   if(this.senType[i].manufacturer===info.manufacturer){
+    //     console.log(this.senType[i]);
+    //   }
+    // }
+    // /*************ModelNumber************************/
+    // for(var i=0;i<this.senType.length;i++){
+    //   if(this.senType[i].modelNumber===info.modelNumber){
+    //     console.log(this.senType[i]);
+    //   }
+    // }
+    
+    /**************QUANTITY*************** */
+    console.log(info);
+    if(info.quantity){
+    for(var i=0;i<this.senType.length;i++){
+      if(this.senType[i].quantity===info.quantity){
+        nextIndex=-1;
+        this.noArgs.push(this.senType[i]);
+        // data=defaultArr.push(this.senType[i]);
+        
+      }
+    }
+  }
+    // /*************Unit************************/
+    // for(var i=0;i<this.senType.length;i++){
+    //   if(this.senType[i].unit===info.unit){
+    //     console.log(this.senType[i]);
+    //   }
+    // }
+    
+    /***************NextIndex************/
+   else if(info.index && info.count){
+    var index=Number(info.index);
+    var count=Number(info.count);
+    var len = index+count;
+    //console.log(nextIndex);
+    for(var i=info.index;i<len;i++){
+      //console.log(this.senType[i]);
+      this.noArgs.push(this.senType[i]);
+    }
+    nextIndex=this.noArgs.length;
+  }
+  // nextIndex=noArgs.length;
+
+  else{
+    var def=DEFAULT_COUNT;
+    //if(nextIndex>=0){
+    for(var i=0;i<def;i++){
+      // console.log(this.senType[i]);
+      this.noArgs.push(this.senType[i]);
+    }
+     nextIndex=this.noArgs.length;
+  }
+
+  
+    //console.log(info);
+   
+  
+    return {nextIndex:nextIndex,data:this.noArgs};
   }
   
   /** Subject to validation of search-parameters in info as per
@@ -102,7 +265,65 @@ class Sensors {
   async findSensors(info) {
     const searchSpecs = validate('findSensors', info);
     //@TODO
-    return {};
+    var sensorArr=[];
+    var auxSen=[]
+    var nextIndex=1;
+    this.sen.sort((a, b) => (a.id > b.id) ? 1 : -1)
+    
+
+    /**************************************/
+    if(info.model && info.count){
+    let c=[];
+    //console.log(info.count);
+    for(var i=0; i<this.sen.length;i++){
+      
+      if(this.sen[i].model===info.model){
+          //c++;
+          //console.log(c);
+      
+          //console.log(this.sen[i]);
+          auxSen.push(this.sen[i]);
+      }
+    }
+        //   if(c===info.count){
+        //     break;
+        //   }
+        //   else
+        //   {
+        //     c++;
+        //     console.log(c);
+        //   }
+        //   console.log(this.sen[i]);
+        // }
+        for(var counter=0;counter<info.count;counter++){
+          //console.log(Number(counter)+Number(sensorArr.length));
+          this.sensorArr.push(auxSen[counter]);
+        }
+        nextIndex=this.sensorArr.length;
+      }
+      else if(info.model && info.count && info.index){
+        for(var i=o;i<this.sen.length;i++){
+          if(this.sen[i]===info.model){
+            this.sensorArr.push(this.sen[i]);
+
+          }
+        }
+      }
+      
+      else {
+        /**********Default=5******* */
+    var def=DEFAULT_COUNT;
+    
+    
+    for(var i=0;i<def;i++){
+      //console.log(this.sen[i]);
+      this.sensorArr.push(this.sen[i]);
+    }
+    nextIndex=this.sensorArr.length;
+
+      }
+    //console.log(this.sen);
+    return {nextIndex:nextIndex,data:this.sensorArr};
   }
   
   /** Subject to validation of search-parameters in info as per
@@ -143,6 +364,156 @@ class Sensors {
   async findSensorData(info) {
     const searchSpecs = validate('findSensorData', info);
     //@TODO
+    this.senData.sort((a, b) => (a.timestamp > b.timestamp) ? -1 : 1)
+    /******************************************************************* */
+    var auxData=[];
+    //console.log(this.senData);
+    for(var i=0; i<this.senData.length;i++){
+      
+      if(this.senData[i].sensorId === info.sensorId){
+        auxData.push(this.senData[i]);
+
+      }
+    }
+    //console.log(auxData);
+    var auxID = auxData[0].sensorId;
+
+    for(var i = 0; i < this.sen.length; i++) {
+      if (this.sen[i].id === auxID) {
+        var senIndex = i;
+        var expectedRange = this.sen[i].expected;
+        var model = this.sen[i].model;
+      }
+    }
+
+    // console.log(expectedRange);
+    // console.log(model);
+
+    for(var i = 0; i < this.senType.length; i++) {
+      if (this.senType[i].id === model) {
+        var senTypeIndex = i;
+        var limitRange = this.senType[i].limits;
+      }
+    }
+
+    //console.log(limitRange);
+
+    function inRange(values,exp,lim) {
+      for (var i = 0; i<values.length; i++) {
+        //console.log(values[i]);
+        if(parseFloat(values[i].value)>parseFloat(exp.min) && parseFloat(values[i].value)<parseFloat(exp.max) && parseFloat(values[i].value)>parseFloat(lim.min) && parseFloat(values[i].value)<parseFloat(lim.max)){
+          //console.log(values[i].value);
+          values[i].status= 'ok';
+          delete values[i].sensorId;
+        }
+        else if ((parseFloat(values[i].value) > parseFloat(lim.min) && parseFloat(values[i].value) < parseFloat(exp.min)) || (parseFloat(values[i].value) > parseFloat(exp.max) && parseFloat(values[i].value) < parseFloat(lim.max))){
+          //console.log(values[i].value);
+          values[i].status = 'outOfRange';
+          delete values[i].sensorId;
+        }
+        else{
+          //console.log(values[i].value);
+          values[i].status = 'error';
+          delete values[i].sensorId;
+       }
+     } 
+   }
+    
+    inRange(auxData,expectedRange,limitRange);
+
+    let auxDataCount = [];
+
+    for (var i = 0; i < info.count; i++) {
+      auxDataCount.push(auxData[i]);
+
+    }
+
+    //console.log(auxData);
+    console.log(auxDataCount);
+/************************************************************* */
+    let details = {};
+    if (info.doDetail) {
+      details.data = auxDataCount;
+      details.sensorType = this.senType[senTypeIndex];
+      details.sensor = this.sen[senIndex];
+    }
+
+    console.log(details);
+/******************************************************88 */
+    let curStatus = {};
+    let ar=[];
+    //console.log(typeof(info.statuses));
+    console.log(info.statuses);
+    if (info.statuses) {
+      for (var i = 0; i < auxData.length; i++) {
+        if(auxData[i].status===info.statuses)
+        {
+          ar.push(auxData[i]);
+        }
+      }
+        //console.log(ar);
+        // if (auxData[i].status === info.statuses) {
+        //   curStatus.data = auxData[i];
+        let arr = [];
+        for(var i=0;i<info.count;i++){
+          //console.log(i);
+          arr.push(ar[i]);
+          //console.log(curStatus);
+
+        }
+        curStatus.data = arr;
+        console.log(curStatus);
+        
+    }
+    
+
+    
+    /**************************************************** */
+    
+    //   for(var counter=0;counter<info.count;counter++){
+    //     //console.log(Number(counter)+Number(sensorArr.length));
+    //     this.dataArr.push(auxData[counter]);
+    //   }
+
+  //     function inRange(value,senType,sen){
+
+  //     if((value<sen.expected.max && value>sen.expected.min) && (value<senType.limits.max && value>senType.limits.min))
+  //     {
+  //       return "OK";
+  //     }
+  //     if((this.senData.value>this.sen.expected.max && this.senData.value>this.senType.limits.max) && (this.senData.value>this.sen.expected.min && this.senData.value>this.senType.limits.min))
+  //     {
+  //       return "Error";
+  //     }
+  //     if((this.senData.value<this.sen.expected.min && this.senData.value>this.sen.expected.max) && (this.senData.value>this.sen.expected.max && this.senData.value<this.senType.limits.max))
+  //     {
+  //       return "Out of Range";
+  //   }
+  // }
+
+    // for(var i=0; i<this.senData.length;i++){
+    //   var status2=inRange(this.senData[i].value,this.senType,this.Sen);
+    //   this.senData[i].status=status2;
+    // }
+    //   for(let j of this.senData){
+    //    var status2=inRange(j.value,this.senType,this.sen);
+    //    j.status=status2;
+    //  }
+    // var exp;
+    // for(let j of this.senData){
+    //     for(var i=0;i<this.sen.length;j++)
+    //     {
+    //       if(j.sensorId===this.sen[i].id)
+    //       {
+    //         exp=this.sen[i].expected;
+    //         console.log(exp);
+            
+    //       }
+    //     }
+    //    }
+       
+
+    
     return {};
   }
   
