0bb348f4308a4f3608ea7bf13a0d1b4075309a0d
Pranjal Kamlesh Parekh <pparekh1@CS444-pparekh1.pods.bu.int>
2019-10-27 00:12:13 -0400

    implemented 2 logs

--- work/prj3-sol/sensors-ws.js
+++ work/prj3-sol/sensors-ws.js
@@ -11,10 +11,114 @@ const NOT_FOUND = 404;
 const CONFLICT = 409;
 const SERVER_ERROR = 500;
 
-function serve(port, sensors) {
+function serve(port,sensors) {
   //@TODO set up express app, routing and listen
+  const app = express();
+  app.locals.port = port;
+  // app.locals.base = base;
+  // app.locals.model = model;
+  app.locals.sensors = sensors;
+  setupRoutes(app);
+  app.listen(port, function() {
+    console.log(`listening on port ${port}`);
+  });
 }
 
-module.exports = { serve };
+module.exports = { serve : serve};
 
 //@TODO routing function, handlers, utility functions
+
+function setupRoutes(app) {
+  //const base = app.locals.base;
+  app.use(cors());
+  app.use(bodyParser.json());
+  app.get('/sensor-types', getsensortype(app));
+ // app.post(base, doCreate(app));
+app.get(`${'/sensor-types'}/:id`, doGet(app));
+  // app.delete(`${base}/:id`, doDelete(app));
+  // app.put(`${base}/:id`, doReplace(app));
+  // app.patch(`${base}/:id`, doUpdate(app));
+ app.use(doErrors()); //must be last   
+}
+
+
+function getsensortype(app) {
+  return errorWrap(async function(req, res) {
+    const q = req.query || {};
+    try {
+      const results = await app.locals.sensors.findSensorTypes(q);
+      res.json(results);
+    }
+    catch (err) {
+      const mapped = mapError(err);
+      res.status(mapped.status).json(mapped);
+    }
+  });
+}
+
+function doGet(app) {
+  return errorWrap(async function(req, res) {
+    try {
+      const id = req.params.id;
+      const results = await app.locals.sensors.findSensorTypes({ id: id });
+      if (results.length === 0) {
+	throw {
+	  isDomain: true,
+	  errorCode: 'NOT_FOUND',
+	  message: `user ${id} not found`,
+	};
+      }
+      else {
+	res.json(results);
+      }
+    }
+    catch(err) {
+      const mapped = mapError(err);
+      res.status(mapped.status).json(mapped);
+    }
+  });
+}
+
+
+function doErrors(app) {
+  return async function(err, req, res, next) {
+    res.status(SERVER_ERROR);
+    res.json({ code: 'SERVER_ERROR', message: err.message });
+    console.error(err);
+  };
+}
+
+function errorWrap(handler) {
+  return async (req, res, next) => {
+    try {
+      await handler(req, res, next);
+    }
+    catch (err) {
+      next(err);
+    }
+  };
+}
+
+/*************************** Mapping Errors ****************************/
+
+const ERROR_MAP = {
+  EXISTS: CONFLICT,
+  NOT_FOUND: NOT_FOUND
+}
+
+/** Map domain/internal errors into suitable HTTP errors.  Return'd
+ *  object will have a "status" property corresponding to HTTP status
+ *  code.
+ */
+function mapError(err) {
+  console.error(err);
+  return err.isDomain
+    ? { status: (ERROR_MAP[err.errCode] || BAD_REQUEST),
+	code: err.errorCode,
+	message: err.message
+      }
+    : { status: SERVER_ERROR,
+	code: 'INTERNAL',
+	message: err.toString()
+      };
+} 
