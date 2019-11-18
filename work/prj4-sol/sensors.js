0402ecc4b9dc4efe8bf28c12c948f2a38636f387
Pranjal Kamlesh Parekh <pparekh1@CS444-pparekh1.pods.bu.int>
2019-11-18 02:10:25 -0500

    home page with some progress

--- work/prj4-sol/sensors.js
+++ work/prj4-sol/sensors.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 const querystring = require('querystring');
 
-const Mustache = require('./mustache');
+const mustache = require('mustache');
 const widgetView = require('./widget-view');
 
 const STATIC_DIR = 'statics';
@@ -14,9 +14,110 @@ const TEMPLATES_DIR = 'templates';
 
 function serve(port, model, base='') {
   //@TODO
+  const app = express();
+  app.locals.port = port;
+  app.locals.base = base;
+  app.locals.model = model;
+  process.chdir(__dirname);
+  app.use(base, express.static(STATIC_DIR));
+  setupTemplates(app);
+  setupRoutes(app);
+  app.listen(port, function() {
+    console.log(`listening on port ${port}`);
+  });
 }
 
 
 module.exports = serve;
 
 //@TODO
+
+const URLS = {
+  sensorType: '/sensorType/:id.html',
+ 
+};
+
+
+function setupRoutes(app) {
+  const base = app.locals.base;
+  //app.get(URLS.sensorType, getSensorType(app));
+  app.get(`/tst-sensor-types-search.html`, createUserForm(app));
+ 
+}
+function createUserForm(app) {
+  return async function(req, res) {
+    const model = { base: app.locals.base, fields: FIELDS };
+    const html = doMustache(app, 'tst-sensor-types-search', model);
+    res.send(html);
+  };
+};
+
+const FIELDS_INFO = {
+  id: {
+    friendlyName: 'Sensor Type Id',
+    isSearch: true,
+    isId: true,
+    //isRequired: true,
+    regex: /^\w+$/,
+    error: 'User Id field can only contain alphanumerics or _',
+  },
+  model: {
+    friendlyName: 'Model Number',
+    isSearch :true,
+    regex: /^\w+$/,
+    error: 'User Id field can only contain alphanumerics or _',
+  },
+  manufacturer: {
+    friendlyName: 'Manufacturer',
+    isSearch :true,
+    regex: /^\w+$/,
+    error: 'User Id field can only contain alphanumerics or _',
+  }
+};
+const FIELDS =
+  Object.keys(FIELDS_INFO).map((n) => Object.assign({name: n}, FIELDS_INFO[n]));
+
+
+
+
+/************************ General Utilities ****************************/
+
+/** Decode an error thrown by web services into an errors hash
+ *  with a _ key.
+ */
+function wsErrors(err) {
+  const msg = (err.message) ? err.message : 'web service error';
+  console.error(msg);
+  return { _: [ msg ] };
+}
+
+function doMustache(app, templateId, view) {
+  const templates = { footer: app.templates.footer };
+  return mustache.render(app.templates[templateId], view, templates);
+}
+
+function errorPage(app, errors, res) {
+  if (!Array.isArray(errors)) errors = [ errors ];
+  const html = doMustache(app, 'errors', { errors: errors });
+  res.send(html);
+}
+
+function isNonEmpty(v) {
+  return (v !== undefined) && v.trim().length > 0;
+}
+
+function setupTemplates(app) {
+  app.templates = {};
+  for (let fname of fs.readdirSync(TEMPLATES_DIR)) {
+    const m = fname.match(/^([\w\-]+)\.ms$/);
+    if (!m) continue;
+    try {
+      app.templates[m[1]] =
+	String(fs.readFileSync(`${TEMPLATES_DIR}/${fname}`));
+    }
+    catch (e) {
+      console.error(`cannot read ${fname}: ${e}`);
+      process.exit(1);
+    }
+  }
+}
